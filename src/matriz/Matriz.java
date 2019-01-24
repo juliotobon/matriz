9f18c0428e9121d321847affa2883504953b8328
Julio Cesar Tobon <Juceto@gmail.com>
2019-01-24 16:36:48 -0500

    "commit 5"

---
+++ src/matriz/Matriz.java
@@ -0,0 +1,98 @@
+package matriz;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Matriz {
+	
+	public static int filas, columnas;
+	
+	public static Entrada IngresarParametros() {
+		
+		Entrada entradas;
+		//int filas,columnas;
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("ingrese el numero de filas que quiere para su matriz");
+		filas= (entrada.nextInt());
+		System.out.println("ingrese el numero de filas que quiere para su matriz");
+		columnas= (entrada.nextInt());
+		
+		Entrada miEntradas= new Entrada(filas, columnas);
+		
+		
+		return miEntradas;
+		
+	}
+	
+	public static int [][] llenarMatriz(int filas, int columnas){
+		
+		int aux ,aux1;
+		int [][] matriz = new int [filas][columnas];
+		Random rnd=new Random();
+		
+		
+	for (int i=0; i<filas; i++) {
+			
+			for(int j=0; j<columnas; j++){
+			
+				aux=  1+rnd.nextInt(4);
+				matriz [i][j]= aux;
+				
+				for(int k=i;k==0;k-- )
+				{
+					for(int l=j;l==0;l--) {
+						
+						if(aux==matriz[k][l] )	{
+							
+							aux1=  1+rnd.nextInt(4);
+							matriz[k][l]= aux1;
+						
+						}
+					}
+				}
+			
+			
+				
+			
+				}
+				
+		}
+
+		
+		return matriz;}
+	
+	
+	public static int [][] verificarMatriz (int filas, int columnas)
+	{
+		int [][] matrizverificada= new int [filas][columnas];
+		
+		
+		
+		return matrizverificada;
+	}
+	
+	public static void mostrarMatrizLlena (int matriz [][]) {
+		
+		
+		  for (int i=0; i<filas; i++) {
+				
+			     for(int j=0; j<columnas; j++){
+					
+					System.out.print(matriz[i][j]+"\t");
+				}
+			     System.out.println("\n");
+		   }
+	}
+	
+
+	public static void main(String[] args) {
+		 
+		//https://github.com/juliotobon/matriz.git
+		
+		IngresarParametros();
+		mostrarMatrizLlena(llenarMatriz(filas, columnas));		
+	
+		
+	}
+
+}
